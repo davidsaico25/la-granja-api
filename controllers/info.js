a09bced8cf56a70a70def9e74096f78150a1f1ad
david benjamin saico paucar <davidsaico25@gmail.com>
2018-03-23 11:15:43 -0500

    testv2

--- controllers/info.js
+++ controllers/info.js
@@ -3,7 +3,7 @@
 var InfoController = () => { };
 
 InfoController.info = (req, res) => {
-    return res.status(200).send({ message: 'Info La-Granja v1' });
+    return res.status(200).send({ message: 'Info La-Granja v2' });
 }
 
 module.exports = InfoController;
