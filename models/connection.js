60dbce32b35cf2f8e4fba522ca34a82b732e894a
david benjamin saico paucar <davidsaico25@gmail.com>
2018-03-23 17:58:56 -0500

    v4

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: '35.199.92.180',
-    user: 'davisonsp',
+    user: 'root',
     password: 'davisonsp',
     database: 'db_la_granja'
 });
